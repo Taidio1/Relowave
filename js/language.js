ee6d95afb5beac95ec5b798a543089ae2ae19357
Zuzanna <80766251+Taidio1@users.noreply.github.com>
2024-06-29 14:09:21 +0200

    Main Templatee Push

---
+++ js/language.js
@@ -0,0 +1,307 @@
+const languages = {
+    pl: {
+      //Home Section
+      //Nav
+      stronaglowna:"Strona Główna",
+      onas:"O Nas",
+      uslugi:"Usługi",
+      zespol:"Zespół",
+      kontakt:"Kontakt",
+      //Welocome Page
+      Tytul: "Witaj w Easy Move!",
+      slogan: "Twój spokój, nasza misja.",
+      btnPoznajNas:"Czytaj więcej",
+      btnKontakt:"Skontaktuj się", //NOWY !!!!!
+
+      //About Us Section
+      tytulAU:"O NAS",
+      podtytulAU:"Dowiedz się, jak nasze doświadczenie może ułatwić Twoją legalną przeprowadzkę do Polski.",
+      Opis1AU:"Z pasji do wspierania osób w procesie przeprowadzki do Polski oraz zrozumienia ich unikalnych potrzeb, powstała nasza firma. Dzięki ponad 5-letniemu doświadczeniu w pracy z Urzędem Wojewódzkim, tworzymy efektywne plany, ułatwiając naszym klientom proces legalizacji pobytu. Zajmujemy się wszystkimi aspektami związanymi z relokacją, zapewniając kompleksową pomoc.",
+      Opis2AU:"Nasze usługi obejmują: doradztwo w procesie legalizacji pobytu, wsparcie w przygotowaniu dokumentów, oraz profesjonalną obsługę administracyjną. Dzięki nam, procedura legalizacji staje się bezproblemowa, pozostawiając klientom mniej trosk związanych z przeprowadzką.",
+
+      //Services
+      Btn1SE:"Legalizacja Pobytu",
+      Tytul1SE:"Oferujemy profesjonalne wsparcie w procesie legalizacji pobytu w Polsce, eliminując potencjalne trudności.",
+      Lista1SE:"Analizę Twojej sytuacji pobytowej",
+      Lista2SE:"Przygotowanie indywidualnego planu przeprowadzenia procedury legalizacji pobytu,",
+      Lista3SE:"Rezerwację terminu do złożenia wniosku,",
+      Lista4SE:"Wsparcie w procesie kompletowania dokumentów,",
+      Lista5SE:"Wspólną wizytę w Urzędzie Wojewódzkim w celu złożenia wniosku pobytowego,",
+      Lista6SE:"Kontakt z Twoim pracodawcą w celu minimalizacji pojawienia się błędów oraz usprawnienia procesu kompletowania dokumentów",
+
+      Btn2SE:"Legalizacja Zatrudnienia",
+      Tytul2SE:"Zapewniamy profesjonalną pomoc w procesie legalizacji zatrudnienia w Polsce, optymalizując czas procedury.",
+      Lista7SE:"Kontrolę statusu zatrudnienia Twoich pracowników,",
+      Lista8SE:"Złożenie wniosku o wydanie informacji starosty,",
+      Lista9SE:"Optymalizację procedury aplikacji o wydanie informacji starosty,",
+      Lista10SE:"Złożenie wniosku o wydanie zezwolenia na pracę,",
+      Lista11SE:"Stały kontakt z inspektorem prowadzącym postępowanie w celu uzyskania informacji na temat toczącego się postępowania,",
+      Lista12SE:"Kontakt z Twoim pracownikiem w celu minimalizacji pojawienia się błędów oraz usprawnienia procesu kompletowania dokumentów",
+
+      OpisSE:"Nasze wsparcie obejmuje:",
+      BtnMoreSE:"Sprawdź więcej",
+
+      //All Services
+      TytulAS:"NASZE USŁUGI",
+      OpisAS:"Skontaktuj się z nami, aby poznać szczegóły na temat poniższych usług:",
+      Btn1AS:"Legalizacja pobytu",
+      Btn2AS:"Legalizacja zatrudnienia",
+      Btn3AS:"Prawo jazdy",
+      Btn4AS:"Zameldowanie",
+      Btn5AS:"Zaświadczenie o niekaralności",
+      Btn6AS:"Uznanie zagranicznych aktów stanu cywilnego",
+      Btn7AS:"Uznanie obywatelstwa polskiego",
+      Btn8AS:"Nadanie obywatelstwa polskiego",
+      
+      //Opinions
+      TytulOP:"OPINIE",
+      OpisOP:"Nasze klienty są naszą najlepszą wizytówką. Sprawdź, co mówią o nas inni!",
+
+      //Team
+      TytulTE:"TEAM",
+      OpisTE:"Poznaj nasz zespół, gotowy służyć Ci profesjonalną pomocą i doświadczeniem w procesie legalizacji.",
+      Rola1TE:"Owner | Specjalista ds. relokacji i imigracji",
+      Rola2TE:"Młodsza Specjalistka ds. legalizacji pobytu",
+      //Rola3TE:"Administrator systemów operacyjnych",
+
+      //Contact
+      TytulCO:"KONTAKT",
+      AdressCO:"Adres",
+      TelefonCO:"Telefon",
+      //Formularz
+      ImieiNazCO:"Imię i Nazwisko",
+      TematCO:"Temat",
+      WiadomoscCO:"Wiadomość",
+      BtnWiadCO:"Wyślij wiadomość",
+
+      //Footer
+      TelefonFO:"Telefon:",
+      //Menu footer
+      stronaglownaFO:"Strona główna",
+      onasFO:"O Nas",
+      uslugiFO:"Usługi",
+      zespolFO:"Zespół",
+      politykaprivFO:"Polityka prywatności",
+      //uslugi footer
+      tytulFO:"Usługi",
+      LegPobFO:"Legalizacja pobytu",
+      LegZatFO:"Legalizacja zatrudnienia",
+      ZasFO:"Zaświadczenie o niekaralności",
+      PrawoJazFO:"Prawo Jazdy",
+      ZamelFO:"Zameldowanie",
+      NadanieObywFO:"Nadanie obywatelstwa polskiego"      
+    },
+    //------------------ENG------------------------
+    en: {
+        //Home Section
+      //Nav
+      stronaglowna:"Home Page",
+      onas:"About Us",
+      uslugi:"Services",
+      zespol:"Team",
+      kontakt:"Contact",
+      //Welocome Page
+      Tytul: "Welcome to Easy Move!",
+      slogan: "Your peace, our mission.",
+      btnPoznajNas:"Meet Us",
+
+      //About Us Section
+      tytulAU:"About Us",
+      podtytulAU:"Learn how our experience can ease your legal relocation to Poland",
+      Opis1AU:"Our company was born from a passion for supporting people in the process of moving to Poland and understanding their unique needs. With over five years of experience working with the Provincial Office, we create effective plans, facilitating our clients' residence legalization process. We handle all aspects of relocation, providing comprehensive assistance.",
+      Opis2AU:"Our services include: advice on the residence legalization process, support in preparing documents, and professional administrative assistance. Thanks to us, the legalization process becomes hassle-free, leaving clients with fewer concerns about relocating.",
+
+      //Services
+      Btn1SE:"Residence Legalization",
+      Tytul1SE:"We offer professional support in the residence legalization process in Poland, eliminating potential difficulties.",
+      Lista1SE:"Analysis of Your Residence Situation",
+      Lista2SE:"Preparation of an individual plan for carrying out the residence legalization procedure,",
+      Lista3SE:"Scheduling an appointment to submit the application,",
+      Lista4SE:"Support in the process of compiling documents,",
+      Lista5SE:"Joint visit to the Provincial Office to submit the residence application,",
+      Lista6SE:"Contact with your employer to minimize errors and streamline the document compilation process",
+
+      Btn2SE:"Employment Legalization",
+      Tytul2SE:"We provide professional assistance in the employment legalization process in Poland, optimizing the procedure time.",
+      Lista7SE:"Control of your employees' employment status",
+      Lista8SE:"Submission of an application for issuing a county official's information,",
+      Lista9SE:"Optimization of the application procedure for issuing county official's information,",
+      Lista10SE:"Submission of an application for a work permit,",
+      Lista11SE:"Constant contact with the inspector conducting the proceedings to obtain information about the ongoing procedure,",
+      Lista12SE:"Contact with your employee to minimize errors and streamline the document compilation process",
+
+      OpisSE:"Our support includes:",
+      BtnMoreSE:"Check more",
+
+      //All Services
+      TytulAS:"Our Services",
+      OpisAS:"Contact us to learn more about the following services:",
+      Btn1AS:"Residence Legalization",
+      Btn2AS:"Employment Legalization",
+      Btn3AS:"Driver's License",
+      Btn4AS:"Registration",
+      Btn5AS:"Certificate of No Criminal Record",
+      Btn6AS:"Recognition of Foreign Civil Status Acts",
+      Btn7AS:"Recognition of Polish Citizenship",
+      Btn8AS:"Granting of Polish Citizenship",
+      
+      //Opinions
+      TytulOP:"REVIEWS",
+      OpisOP:"Our clients are our best business card. Check out what others are saying about us!",
+
+      //Team
+      TytulTE:"TEAM",
+      OpisTE:"Meet our team, ready to serve you with professional assistance and experience in the legalization process..",
+      Rola1TE:"Owner | Relocation and Immigration Specialist",
+      Rola2TE:"Junior Residence Legalization Specialist",
+      //Rola3TE:"Operating Systems Administrator",
+
+      //Contact
+      TytulCO:"Contact",
+      AdressCO:"Address",
+      TelefonCO:"Phone",
+      //Formularz
+      ImieiNazCO:"Name and Surname",
+      TematCO:"Subject",
+      WiadomoscCO:"Message",
+      BtnWiadCO:"Send Message",
+
+      //Footer
+      TelefonFO:"Phone:",
+      //Menu footer
+      stronaglownaFO:"Home Page",
+      onasFO:"About Us",
+      uslugiFO:"Services",
+      zespolFO:"Team",
+      politykaprivFO:"Privacy Policy",
+      //uslugi footer
+      tytulFO:"Services",
+      LegPobFO:"Residence Legalization",
+      LegZatFO:"Employment Legalization",
+      ZasFO:"Certificate of No Criminal Record",
+      PrawoJazFO:"Driver's License",
+      ZamelFO:"Registration",
+      NadanieObywFO:"Granting of Polish Citizenship" 
+    }
+  };
+
+  // Początkowy język ustawiony na polski
+//let currentLanguage = 'pl';
+
+function updateTexts(language) {
+  //Welocome Page
+  document.getElementById('Tytul').innerText = languages[language].Tytul;
+  document.getElementById('slogan').innerText = languages[language].slogan;
+  document.getElementById('btnPoznajNas').innerText = languages[language].btnPoznajNas;
+  //Nav
+  document.getElementById('stronaglowna').innerText = languages[language].stronaglowna;
+  document.getElementById('onas').innerText = languages[language].onas;
+  document.getElementById('uslugi').innerText = languages[language].uslugi;
+  document.getElementById('zespol').innerText = languages[language].zespol;
+  document.getElementById('kontakt').innerText = languages[language].kontakt;
+  //About Us Section
+  document.getElementById('tytulAU').innerText = languages[language].tytulAU;
+  document.getElementById('podtytulAU').innerText = languages[language].podtytulAU;
+  document.getElementById('Opis1AU').innerText = languages[language].Opis1AU;
+  document.getElementById('Opis2AU').innerText = languages[language].Opis2AU;
+  //Servieces 1
+  document.getElementById('Btn1SE').innerText = languages[language].Btn1SE;
+  document.getElementById('Btn2SE').innerText = languages[language].Btn2SE;
+  document.getElementById('Tytul1SE').innerText = languages[language].Tytul1SE;
+  document.getElementById('Lista1SE').innerText = languages[language].Lista1SE;
+  document.getElementById('Lista2SE').innerText = languages[language].Lista2SE;
+  document.getElementById('Lista3SE').innerText = languages[language].Lista3SE;
+  document.getElementById('Lista4SE').innerText = languages[language].Lista4SE;
+  document.getElementById('Lista5SE').innerText = languages[language].Lista5SE;
+  document.getElementById('Lista6SE').innerText = languages[language].Lista6SE;
+  //Services 1 main
+  document.getElementById('OpisSE').innerText = languages[language].OpisSE;
+  document.getElementById('BtnMoreSE').innerText = languages[language].BtnMoreSE;
+  //Servieces 2
+  document.getElementById('Tytul2SE').innerText = languages[language].Tytul2SE;
+  document.getElementById('Lista7SE').innerText = languages[language].Lista7SE;
+  document.getElementById('Lista8SE').innerText = languages[language].Lista8SE;
+  document.getElementById('Lista9SE').innerText = languages[language].Lista9SE;
+  document.getElementById('Lista10SE').innerText = languages[language].Lista10SE;
+  document.getElementById('Lista11SE').innerText = languages[language].Lista11SE;
+  document.getElementById('Lista12SE').innerText = languages[language].Lista12SE;
+  //Servieces 2 main
+  document.getElementById('Opis2SE').innerText = languages[language].OpisSE;
+  document.getElementById('BtnMore2SE').innerText = languages[language].BtnMoreSE;
+  
+  //All Services
+  document.getElementById('TytulAS').innerText = languages[language].TytulAS;
+  document.getElementById('OpisAS').innerText = languages[language].OpisAS;
+  document.getElementById('Btn1AS').innerText = languages[language].Btn1AS;
+  document.getElementById('Btn2AS').innerText = languages[language].Btn2AS;
+  document.getElementById('Btn3AS').innerText = languages[language].Btn3AS;
+  document.getElementById('Btn4AS').innerText = languages[language].Btn4AS;
+  document.getElementById('Btn5AS').innerText = languages[language].Btn5AS;
+  document.getElementById('Btn6AS').innerText = languages[language].Btn6AS;
+  document.getElementById('Btn7AS').innerText = languages[language].Btn7AS;
+  document.getElementById('Btn8AS').innerText = languages[language].Btn8AS;
+  //Opinions
+  document.getElementById('TytulOP').innerText = languages[language].TytulOP;
+  document.getElementById('OpisOP').innerText = languages[language].OpisOP;
+  //Team
+  document.getElementById('TytulTE').innerText = languages[language].TytulTE;
+  document.getElementById('OpisTE').innerText = languages[language].OpisTE;
+  document.getElementById('Rola1TE').innerText = languages[language].Rola1TE;
+  document.getElementById('Rola2TE').innerText = languages[language].Rola2TE;
+  //document.getElementById('Rola3TE').innerText = languages[language].Rola3TE;
+  
+  //Contact
+  document.getElementById('TelefonCO').innerText = languages[language].TelefonCO;
+  document.getElementById('AdressCO').innerText = languages[language].AdressCO;
+    //Formularz
+    document.getElementById('name').placeholder = languages[language].ImieiNazCO;
+    document.getElementById('subject').placeholder = languages[language].TematCO;
+    document.getElementById('WiadomoscCO').placeholder = languages[language].WiadomoscCO;
+    document.getElementById('BtnWiadCO').innerText = languages[language].BtnWiadCO;
+  //Footer
+  document.getElementById('TelefonFO').innerText = languages[language].TelefonFO;
+    //Menu Footer
+  document.getElementById('stronaglownaFO').innerText = languages[language].stronaglownaFO;
+  document.getElementById('onasFO').innerText = languages[language].onasFO;
+  document.getElementById('uslugiFO').innerText = languages[language].uslugiFO;
+  document.getElementById('zespolFO').innerText = languages[language].zespolFO;
+  document.getElementById('politykaprivFO').innerText = languages[language].politykaprivFO;
+    //Uslugi Footer
+  document.getElementById('tytulFO').innerText = languages[language].tytulFO;
+  document.getElementById('LegPobFO').innerText = languages[language].LegPobFO;
+  document.getElementById('LegZatFO').innerText = languages[language].LegZatFO;
+  document.getElementById('ZasFO').innerText = languages[language].ZasFO;
+  document.getElementById('PrawoJazFO').innerText = languages[language].PrawoJazFO;
+  document.getElementById('ZamelFO').innerText = languages[language].ZamelFO;
+  document.getElementById('NadanieObywFO').innerText = languages[language].NadanieObywFO;
+
+
+}
+document.getElementById('language-toggle').addEventListener('change', function() {
+    const language = this.checked ? 'en' : 'pl';
+    localStorage.setItem('preferredLanguage', language);
+    updateTexts(language);
+  });
+
+  
+  // Uaktualnij teksty na początkowy język (polski) po załadowaniu strony
+  window.onload = () => {
+    // Odczytaj zapisany język, domyślnie 'pl' (polski)
+    const savedLanguage = localStorage.getItem('preferredLanguage') || 'pl';
+    updateTexts(savedLanguage);
+  
+    // Ustaw stan przełącznika zgodnie z zapisanym językiem
+    document.getElementById('language-toggle').checked = savedLanguage === 'en';
+  };
+
+  //Opcja z przyciskiem
+  /*
+  document.getElementById('changeLanguage').addEventListener('click', () => {
+    // Zmiana języka
+    currentLanguage = currentLanguage === 'pl' ? 'en' : 'pl';
+    updateTexts();
+  });
+  
+  // Uaktualnij teksty po załadowaniu strony
+  window.onload = updateTexts;
+  */
